b77ca2017f1c1d76a9106cdfae899ebbc401dafb
israelGitV1 <kasper.war@gmail.com>
2025-01-29 15:49:07 -0300

    feat: add compoments Button and Input

---
+++ src/Components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input {
+        width: 100%;
+        height: 75%;
+        background-color: #AAAAFF;
+        border: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px;
+        color: #FFFFFF
+
+    }
+`
